9420f1c219ff583cc2d1d2b720fc885b5d4d8f46
= <=>
2014-07-04 00:14:57 +0800

    defination of node

--- node.py
+++ node.py
@@ -5,15 +5,13 @@ from krpc import DHTProtocol
 from utility import generate_node_id
 
 
-NODE_ID_LENGTH = 20
-
 class Node(object):
-    def __init__(self, node_id = None, rtable = []):
+    def __init__(self, node_id = None, rtable = [], addr = None):
         if node_id == None:
             self.node_id = generate_node_id()
         else:
             self.node_id = node_id
         
         self.rtable = rtable
-        self.protocol = DHTProtocol(self.node_id, self.rtable)    
+        self.protocol = DHTProtocol(self.node_id, self.rtable, addr)    
 
